a5014d2ba64be53c49201f1571ca812fdf52c4d7
pedroalegre <alegre.p@gmail.com>
2017-03-08 09:58:39 +0100

    Sign in existing users (not finished)

--- app/src/main/java/com/ubiqumproject/ubiqumproject/MainActivity.java
+++ app/src/main/java/com/ubiqumproject/ubiqumproject/MainActivity.java
@@ -1,8 +1,75 @@
 package com.ubiqumproject.ubiqumproject;
 
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import static android.content.ContentValues.TAG;
+
 /**
  * Created by Pedro on 01-Mar-17.
  */
 
-public class MainActivity {
+public class MainActivity extends AppCompatActivity {
+	private FirebaseAuth mAuth;
+	private FirebaseAuth.AuthStateListener mAuthListener;
+
+	private String email;
+	private String password;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		mAuth = FirebaseAuth.getInstance();
+
+		mAuthListener = new FirebaseAuth.AuthStateListener() {
+			@Override
+			public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+				FirebaseUser user = firebaseAuth.getCurrentUser();
+				if(user != null) {
+					//User is signed in
+					Log.d(TAG, "onAuthStateChanged:signed_in" + user.getUid());
+				} else {
+					// User is signed out
+					Log.d(TAG, "onAuthStateChanged:signed_out");
+				}
+			}
+		};
+
+		mAuth.signInWithEmailAndPassword(email, password)
+				.addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+					@Override
+					public void onComplete(@NonNull Task<AuthResult> task) {
+						Log.d(TAG, "singInWithEmail:onComplete:" + task.isSuccessful());
+
+						if(!task.isSuccessful()) {
+							Log.w(TAG, "signInWithEmail:failed", task.getException());
+							Toast.makeText(EmailPasswordActivity.this, R.string.auth_failed,
+									Toast.LENGTH_SHORT).show();
+						}
+					}
+				});
+	}
+
+	@Override
+	public void onStart() {
+		super.onStart();
+		mAuth.addAuthStateListener(mAuthListener);
+	}
+
+	@Override
+	public void onStop() {
+		super.onStop();
+		if(mAuthListener != null) {
+			mAuth.removeAuthStateListener(mAuthListener);
+		}
+	}
 }
