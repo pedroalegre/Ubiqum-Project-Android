0f07be59fef0ed71290b076f7c02741662b5fbda
pedroalegre <alegre.p@gmail.com>
2017-03-14 12:40:25 +0100

    Authentication with Firebase done

--- app/src/main/java/com/ubiqumproject/ubiqumproject/MainActivity.java
+++ app/src/main/java/com/ubiqumproject/ubiqumproject/MainActivity.java
@@ -3,7 +3,10 @@ package com.ubiqumproject.ubiqumproject;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -18,18 +21,27 @@ import static android.content.ContentValues.TAG;
  * Created by Pedro on 01-Mar-17.
  */
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements
+        View.OnClickListener {
+
 	private FirebaseAuth mAuth;
 	private FirebaseAuth.AuthStateListener mAuthListener;
 
-	private String email;
-	private String password;
+	private EditText emailField;
+	private EditText passwordField;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
+
+        // views
+        emailField = (EditText) findViewById(R.id.field_email);
+        passwordField = (EditText) findViewById(R.id.field_password);
+
+        // buttons
+        findViewById(R.id.sign_in_button).setOnClickListener(this);
+
 		mAuth = FirebaseAuth.getInstance();
 
 		mAuthListener = new FirebaseAuth.AuthStateListener() {
@@ -46,19 +58,7 @@ public class MainActivity extends AppCompatActivity {
 			}
 		};
 
-		mAuth.signInWithEmailAndPassword(email, password)
-				.addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-					@Override
-					public void onComplete(@NonNull Task<AuthResult> task) {
-						Log.d(TAG, "singInWithEmail:onComplete:" + task.isSuccessful());
 
-						if(!task.isSuccessful()) {
-							Log.w(TAG, "signInWithEmail:failed", task.getException());
-							Toast.makeText(MainActivity.this, R.string.auth_failed,
-									Toast.LENGTH_SHORT).show();
-						}
-					}
-				});
 	}
 
 	@Override
@@ -74,4 +74,55 @@ public class MainActivity extends AppCompatActivity {
 			mAuth.removeAuthStateListener(mAuthListener);
 		}
 	}
+
+	private void signIn(String email, String password) {
+        Log.d(TAG, "signIn:" + email);
+        if (!validateForm()) {
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "singInWithEmail:onComplete:" + task.isSuccessful());
+
+                        if(!task.isSuccessful()) {
+                            Log.w(TAG, "signInWithEmail:failed", task.getException());
+                            Toast.makeText(MainActivity.this, R.string.auth_failed,
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+	private boolean validateForm() {
+        boolean valid = true;
+
+        String email = emailField.getText().toString();
+        if(TextUtils.isEmpty(email)) {
+            emailField.setError("Required.");
+            valid = false;
+        } else {
+            emailField.setError(null);
+        }
+
+        String password = passwordField.getText().toString();
+        if(TextUtils.isEmpty(password)) {
+            passwordField.setError("Required.");
+            valid = false;
+        } else {
+            passwordField.setError(null);
+        }
+
+        return valid;
+    }
+
+    @Override
+    public void onClick(View v) {
+        int i = v.getId();
+        if (i == R.id.sign_in_button) {
+            signIn(emailField.getText().toString(), passwordField.getText().toString());
+        }
+    }
 }
